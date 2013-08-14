7fa41b1f43bbbdc447173a222064a46b3d35b866
Hayaki Saito <user@zuse.jp>
2013-08-14 17:26:17 +0900

    Add some missing files

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages, Extension
+import inspect, os
+
+filename = inspect.getfile(inspect.currentframe())
+dirpath = os.path.abspath(os.path.dirname(inspect.getfile(inspect.currentframe())))
+long_description = open(os.path.join(dirpath, "README.rst")).read()
+
 
 __author__  = "Hayaki Saito (user@zuse.jp)"
-__version__ = "0.0.1"
+__version__ = "0.0.4"
 __license__ = "GPL v3"
 
 setup(name                  = 'imageloader',
       version               = __version__,
       description           = 'An image loader library which provides a subset of PIL interface.',
-      long_description      = open("README.rst").read(),
+      long_description      = long_description,
       ext_modules           = [Extension('imageloader', sources = ['imageloader.c'])],
       classifiers           = ['Development Status :: 4 - Beta',
                                'Intended Audience :: Developers',
@@ -22,6 +28,6 @@ setup(name                  = 'imageloader',
       url                   = 'https://github.com/saitoha/python-imageloader',
       license               = __license__,
       zip_safe              = False,
-      include_package_data  = False,
+      include_package_data  = True,
       )
 
